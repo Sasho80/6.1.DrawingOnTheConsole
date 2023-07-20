1e32edaa7fa9ab83ee63b9821632d8fa5d712967
Aleksandar <Sasho80@gbg.bg>
2023-07-20 21:21:23 +0300

    Add files via upload

---
+++ SquareOfStars.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	for  (int r = 0; r < n; r++){
+		cout << "*";
+		for (int c = 0; c < n-1; c++){
+			cout << " *";
+		}
+		cout << endl;
+	}
+	return 0;
+}
