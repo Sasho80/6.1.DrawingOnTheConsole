bd80a3e4e4b76e213255befd234e4e0bc893dfea
Aleksandar <Sasho80@gbg.bg>
2023-08-13 17:29:17 +0300

    Update 06.RhombusOfStars.cpp

--- 06.RhombusOfStars.cpp
+++ 06.RhombusOfStars.cpp
@@ -1,89 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	if (n <= 2) {
-		for (int r = 0; r < n - 1; r++) {
-			for (int c = 0; c < n - r - 1; c++) {
-				cout << " ";  //Print top row
-			}
-			cout << "*";
+	int colsCnt = 1;
+	// first part
+	for (int row = 0; row < n - 1; row++) {
+		cout << string(n - colsCnt,' ');
+
+		for (int col = 0; col < colsCnt; col++) {
+			cout << "* ";
 		}
 		cout << endl;
-		for (int c = 0; c < n; c++) {
-			cout << "*";  //Print mid row
-			cout << " ";
-		}
-		cout << endl;
-		for (int r = 0; r < n - 1; r++) {
-			for (int c = 0; c < n - r - 1; c++) {
-				cout << " "; //Print bottom row
-			}
-			cout << "*";
-		}
-		cout << endl;
+		colsCnt++;
 	}
-	if (n > 2) {
-		for (int r = 0; r < n - (n - 1); r++) {
-			for (int c = 0; c < n - r - 1; c++) {
-				cout << " ";    //Print first row
-			}
-			cout << "*";
-		}
-		cout << endl;
-		for (int r = 1; r <= n - (n - 1); r++) {
-			for (int c = 0; c < n - r - 1; c++) {
-				cout << " ";   //Print second row
-			}
-			cout << "*" << " " << "*";
-		}
-		cout << endl;
-		if (n>3){
-			for (int r = 2; r < n -1; r++) {
-				for (int c = 0; c < n - r - 1; c++) {
-					cout << " ";  
-				}
-				for (int c = 0; c <r+1; c++) {
-					cout << "*"; 
-					cout << " ";
-				}
-				cout << endl;
-			}
-		}
-		for (int r = 0; r < n; r++) {
-			cout << "*";  //Print mid row
-			cout << " ";
-		}
-		if (n > 3) {
-			cout << endl;
-			for (int r = 2; r <n - 1; r++) {
-				for (int c = 0; c < n - r - 1; c++) {
-					cout << " ";   
-				}
-				for (int c = 0; c <r+1; c++) {
-					cout << "*";
-					cout << " ";
-				}
-				cout << endl;
-			}
-		}
-		if (n==3) {
-			cout << endl;
-		}
-		for (int r = 1; r <= n - (n - 1); r++) {
-			for (int c = 0; c < n - r - 1; c++) {
-				cout << " ";     //Print second row
-			}
-			cout << "*" << " " << "*";
-		}
-		cout << endl;
-		for (int r = 0; r < n - (n - 1); r++) {
-			for (int c = 0; c < n - r - 1; c++) {
-				cout << " ";    //Print first row
-			}
-			cout << "*";
-		}
-		cout << endl;
+	// middle part
+	for (int col = 0; col < n; col++){
+		cout << "* ";
 	}
+	cout << endl;
+	//bottom part
+	colsCnt = n-1;
+	for (int row = 0; row < n-1 ; row++) {
+		cout << string(n - colsCnt,' ');
+
+		for (int col = 0; col < colsCnt; col++) {
+			cout << "* ";
+		}
+		cout << endl;
+		colsCnt--;
+	}
+	return 0;
 }
